d55d5d407c3243759000e4ab76e23faa66fc91e8
ichitaso <willfeeltips@gmail.com>
2023-03-15 15:35:12 +0900

    fix little2

--- afc2dSupport/afc2dSupport.mm
+++ afc2dSupport/afc2dSupport.mm
@@ -61,8 +61,7 @@ void patch_setuid() {
     fix_setuid_prt_t ptr = (fix_setuid_prt_t)dlsym(handle, "jb_oneshot_fix_setuid_now");
 
     const char *dlsym_error = dlerror();
-    if (dlsym_error)
-    return;
+    if (dlsym_error) return;
 
     ptr(getpid());
 }
